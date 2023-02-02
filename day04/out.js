a206e6f48f59532a824d85f72691a6e71f4aeed9
rlaqja <124110631+rlaqja@users.noreply.github.com>
2023-02-02 17:34:23 +0900

    2023/0202

---
+++ day04/out.js
@@ -0,0 +1,20 @@
+function 여행하다() {
+    console.log("유럽가기.")
+    console.log("일본가기.")
+    console.log("미국가기.")
+    console.log("중국가기.")
+    console.log("코타키나발ㄹ.")
+    // console.log("6탈수.")
+    // console.log(".")
+}
+여행하다()
+function 쉬다() {
+    console.log("침대에서 쉬기.")
+    console.log("호캉스가기.")
+    // console.log("3비누를 바른다.")
+    // console.log("4오물주물 비빈다.")
+    // console.log("5행군다.")
+    // console.log("6탈수.")
+    // console.log("7건조.")
+}
+쉬다()
